4dc303cb2f91ce2adabbff222a2c5053c08b2e76
Kestrel.Bryce <99274358+KestrelBryce@users.noreply.github.com>
2022-05-03 15:52:56 +0000

    Changes js a bit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![GitHub's Super Linter](https://github.com/KestrelBryce/Unit4-03-HTML-Museum/actions/workflows/main.yml/badge.svg)](https://github.com/KestrelBryce/Unit4-03-HTML-Museum/actions)
+[![GitHub's Super Linter](https://github.com/KestrelBryce/Unit4-03-HTML-Museum/actions/workflows/main.yml/badge.svg)](https://github.com/KestrelBryce/Unit4-03-HTML-Movie/actions)
--- js/script.js
+++ js/script.js
@@ -6,29 +6,25 @@ function displayGreeting () {
     // user input
   let userAge = parseInt((document.getElementById('Inserted-age-here').value));
 
+  let dayWeek = (document.getElementById('Inserted-age-here').value);
   
     // Process
-    // R-RATED
+    // Free admission
   if (userAge >= 18) {
     document.getElementById("greeting").innerHTML = "You are " + userAge + ", which means you are allowed to see an R-rated movie alone."
   }
   
-    // PG-14
+    // Student discount
   else if (userAge >= 14) {
     document.getElementById("greeting").innerHTML = "You are " + userAge + ", which means you are allowed to see a PG-14 movie alone."
   }
 
-    // PG-13
+    // Day discount
   else if (userAge == 13) {
     document.getElementById("greeting").innerHTML = "You are " + userAge + ", which means you are allowed to see a PG-13 movie alone."
   }
 
-    // G/PG
-  else if (userAge >= 10) {
-    document.getElementById("greeting").innerHTML = "You are " + userAge + ", which means you are allowed to see a G/PG movie alone."
-  }
-
-  // No movies for you
+  // Normal admission
   else {
     document.getElementById("greeting").innerHTML = "You are " + userAge + ", which means you are not allowed to see any movies alone."
   }
