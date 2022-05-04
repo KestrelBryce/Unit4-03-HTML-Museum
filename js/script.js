aa96add292731b3424717a8347050562b2342fb4
Kestrel.Bryce <99274358+KestrelBryce@users.noreply.github.com>
2022-05-04 14:36:12 +0000

    changed js again

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-  /* This function compares the user's age to current Canadian age restrictions on movies. 
+  /* This function compares the user's age and the day of the week to discounts at a museum to see if the user is eligable. 
 */
 
 function displayGreeting () {
@@ -6,27 +6,29 @@ function displayGreeting () {
     // user input
   let userAge = parseInt((document.getElementById('Inserted-age-here').value));
 
-  let dayWeek = (document.getElementById('Inserted-age-here').value);
+  let dayWeek = (document.getElementById('insert-Day-Here').value);
+
+    // Remember to write || by using shift on \ twice
   
     // Process
     // Free admission
-  if (userAge >= 18) {
-    document.getElementById("greeting").innerHTML = "You are " + userAge + ", which means you are allowed to see an R-rated movie alone."
+  if ((userAge < 5) || (userAge > 95)) {
+    document.getElementById("greeting").innerHTML = "You are " + userAge + ", which means you are eligable for free admission."
   }
   
     // Student discount
-  else if (userAge >= 14) {
-    document.getElementById("greeting").innerHTML = "You are " + userAge + ", which means you are allowed to see a PG-14 movie alone."
+  else if ((userAge >= 12) && (userAge <= 21)) {
+    document.getElementById("greeting").innerHTML = "You are " + userAge + ", which means you are eligable for a student discount."
   }
 
     // Day discount
-  else if (userAge == 13) {
-    document.getElementById("greeting").innerHTML = "You are " + userAge + ", which means you are allowed to see a PG-13 movie alone."
+  else if ((dayWeek == "Tuesday" || dayWeek == "Thursday")) {
+    document.getElementById("greeting").innerHTML = "It is " + dayWeek + ", which means you are eligable for a discount today."
   }
 
   // Normal admission
   else {
-    document.getElementById("greeting").innerHTML = "You are " + userAge + ", which means you are not allowed to see any movies alone."
+    document.getElementById("greeting").innerHTML = "Sorry, you don't meet any of the requirements for a discount. You need to pay the regular admission fee."
   }
 
 }
